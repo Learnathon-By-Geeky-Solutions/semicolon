35bc5ee91d035714126fe463e7ec6874ee425203
SUMAIYA AHMED RANI <137536299+potate4@users.noreply.github.com>
2025-01-16 14:37:01 +0600

    init: initial project set up with database connection to mongodb

---
+++ backend/dist/index.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(5000, () => {
+        console.log("Server Listening to PORT: ", PORT, ", and Connected to Database");
+    });
+})
+    .catch((err) => {
+    console.log(err);
+});
+//# sourceMappingURL=index.js.map
