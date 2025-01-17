890ca222374a1e7d7ce2ec74f68ebb7b2f70ef0f
SUMAIYA AHMED RANI <137536299+potate4@users.noreply.github.com>
2025-01-17 14:44:29 +0600

    style: removed unnecessary comments and whitespaces

--- backend/src/routes/authorityRoutes.ts
+++ backend/src/routes/authorityRoutes.ts
@@ -1,12 +1,8 @@
 import express  from "express";
 import { test } from "../controllers/authorityController.js";
+
 const authorityRouter = express.Router();
 
-
 authorityRouter.get("/", test);
-// authorityRouter.post("/signup", signup);
-// authorityRouter.post("/login", login);
-// authorityRouter.post("/logout", logout);
-
 
 export default authorityRouter;
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,13 +1,8 @@
 import express  from "express";
 import { test } from "../controllers/userController.js";
+
 const userRouter = express.Router();
 
-
 userRouter.get("/", test);
-
-// userRouter.post("/signup", signup);
-// userRouter.post("/login", login);
-// userRouter.post("/logout", logout);
-
 
 export default userRouter;
