e1f7a2bd4375003283bf5bd11b9bb80a6945aae1
SUMAIYA AHMED RANI <137536299+potate4@users.noreply.github.com>
2025-01-28 18:31:04 +0600

    refactor: refactored the code for shelter mapping

--- frontend/src/pages/shelters.tsx
+++ frontend/src/pages/shelters.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import MapWithClickableCustomMarkers from "../components/googleMaps/markerMap";
+import MapWithShelters from "../components/googleMaps/shelterMap.js";
 
 const Shelters: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 flex">
-        <MapWithClickableCustomMarkers></MapWithClickableCustomMarkers>
+        <MapWithShelters></MapWithShelters>
     </div>
   );
 };
