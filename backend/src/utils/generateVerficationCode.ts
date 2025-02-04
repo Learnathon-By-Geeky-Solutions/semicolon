279db1f362640a95b08233e5fe377207c3b827a3
mehreen019 <mehreenhossain@iut-dhaka.edu>
2025-02-04 15:46:15 +0600

    fix: fixed cryptography security issue

--- backend/src/utils/generateVerficationCode.ts
+++ backend/src/utils/generateVerficationCode.ts
@@ -1,3 +1,13 @@
-export const generateVerificationCode = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
+import crypto from 'crypto';
+
+export const generateVerificationCode = (): string => {
+    const min = 100000;
+    const max = 999999;
+    const range = max - min + 1;
+    
+    const randomBytes = crypto.randomBytes(4);
+    const randomNumber = randomBytes.readUInt32BE(0);
+    const result = min + (randomNumber % range);
+    
+    return result.toString();
 }
